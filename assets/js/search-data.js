c2e033204bc279e7a201eb902ff4ae8542934020
danielkuhlen <99406487+danielkuhlen@users.noreply.github.com>
2025-09-15 14:28:25 +0000

    Deploying to gh-pages from @ danielkuhlen/danielkuhlen.github.io@06335a330e1cbb37887e26e9faaf72562423437c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,12 +9,12 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+  },{id: "nav-publications",
+          title: "Publications",
+          description: "Here you can find my published work.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/projects/";
+            window.location.href = "/publications/";
           },
         },{id: "nav-cv",
           title: "CV",
